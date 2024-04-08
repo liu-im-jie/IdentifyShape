3c2ce6063f95d9f123d0191c799eae43de8e6c01
liu jie <liu.im.jie@gmail.com>
2024-04-08 10:01:21 +0800

    Initial commit

---
+++ src/main/java/cool/islj/identifyshape/IdentifyShapeApplication.java
@@ -0,0 +1,13 @@
+package cool.islj.identifyshape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentifyShapeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentifyShapeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cool/islj/identifyshape/IdentifyShapeApplicationTests.java
@@ -0,0 +1,13 @@
+package cool.islj.identifyshape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentifyShapeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
